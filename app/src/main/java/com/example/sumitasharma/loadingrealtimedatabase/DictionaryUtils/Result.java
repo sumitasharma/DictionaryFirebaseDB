ba1f209e07f0b88f681454eb36b0bd7c12521c86
sumitasharma <30515910+sumitasharma@users.noreply.github.com>
2018-06-23 01:28:50 -0700

    Making some Lint Changes.

--- app/src/main/java/com/example/sumitasharma/loadingrealtimedatabase/DictionaryUtils/Result.java
+++ app/src/main/java/com/example/sumitasharma/loadingrealtimedatabase/DictionaryUtils/Result.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class Result {
+class Result {
 
     @SerializedName("id")
     @Expose
--- app/src/main/java/com/example/sumitasharma/loadingrealtimedatabase/DictionaryUtils/Sense.java
+++ app/src/main/java/com/example/sumitasharma/loadingrealtimedatabase/DictionaryUtils/Sense.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class Sense {
+class Sense {
 
     @SerializedName("definitions")
     @Expose
